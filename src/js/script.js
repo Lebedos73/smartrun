e2286cf3bbe0df460bc077b3454a0815fc68063e
lebedos73 <lebedos73@gmail.com>
2024-02-10 17:16:58 +0600

    added carousel and tabs with animation

--- src/js/script.js
+++ src/js/script.js
@@ -1,16 +1,23 @@
-$(document).ready(function () {
-  $('.carousel__inner').slick({
-    speed: 1000,
-    prevArrow: '<button type="button" class="slick-prev"><img src="icons/left_solid.png"></button>',
-    nextArrow: '<button type="button" class="slick-next"><img src="icons/right_solid.png"></button>',
-    responsive: [
-      {
-        breakpoint: 992,
-        settings: {
-          dots: true,
-          arrows: false
-        }
-      }
-    ]
-  });
+
+var slider = tns({
+  container: '.carousel__inner',
+  items: 1,
+  slideBy: 'page',
+  autoplay: false,
+  controls: false,
+  nav: false,
+  responsive: {
+    991: {
+      items: 1,
+      autoHeight: true
+    },
+  }
+});
+
+document.querySelector('.prev').addEventListener('click', function () {
+  slider.goTo('prev');
+});
+
+document.querySelector('.next').addEventListener('click', function () {
+  slider.goTo('next');
 });
