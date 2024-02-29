3302e125ec157261d9f73c15af53dfecff9af37f
lebedos73 <lebedos73@gmail.com>
2024-02-29 12:30:43 +0600

    pre validation version

--- src/js/script.js
+++ src/js/script.js
@@ -5,6 +5,8 @@ const slider = tns({
   slideBy: 'page',
   autoplay: false,
   controls: false,
+  autoHeight: false,
+  loop: true,
   nav: false,
   responsive: {
     991: {
@@ -25,6 +27,9 @@ document.querySelector('.next').addEventListener('click', function () {
 (function ($) {
   $(function () {
 
+
+    new WOW().init(); //.client animation
+
     $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
       $(this)
         .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
@@ -44,6 +49,97 @@ document.querySelector('.next').addEventListener('click', function () {
 
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
+
+    // Modal 
+
+    $('[data-modal=consultation]').on('click', function () {
+      $('.overlay, #consultation').fadeIn('slow');
+    });
+
+    $('.modalW__close').on('click', function () {
+      $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+    });
+
+    $('.button_mini').each(function (i) {
+      $(this).on('click', function () {
+        $('#order .modalW__descr').text($('.catalog-item__subtitle').eq(i).text());
+        $('.overlay, #order').fadeIn('slow');
+      });
+    });
+
+
+    function validateForms(form) {
+      $(form).validate({
+        rules: {
+          name: {
+            required: true,
+            minlength: 2
+          },
+          phone: "required",
+          email: {
+            required: true,
+            email: true
+          }
+        },
+        messages: {
+          name: {
+            required: "Пожалуйста введите своё имя",
+            minlength: jQuery.validator.format("Введите {0} символа")
+          },
+          phone: "Пожалуйста введите свой номер телефона",
+          email: {
+            required: "Пожалуйста введите свою почту",
+            email: "Неправильно введен адрес почты"
+          }
+        }
+      });
+    };
+
+    validateForms('#consultation-form');
+    validateForms('#consultation form');
+    validateForms('#order form');
+
+    $('input[name=phone]').mask("+7 (999) 999-99-99")
+
+    $('form').submit(function (e) {
+      e.preventDefault();
+
+      if (!$(this).valid()) {
+        return;
+      };
+
+      $.ajax({
+        type: "POST",
+        url: "mailer/smart.php",
+        data: $(this).serialize()
+      }).done(function () {
+        $(this).find("input").val("");
+        $('#consultation, #order').fadeOut();
+        $('form').trigger('reset');
+
+        $('.overlay, #thanks').fadeIn('slow');
+      });
+      return false;
+    });
+
+
+    // Smoth scroll and pageup
+
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 1600) {
+        $('.pageup').fadeIn();
+      } else {
+        $('.pageup').fadeOut();
+      }
+    });
+
+    $("a[href=#up]").click(function () {
+      const _href = $(this).attr("href");
+      $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
+      return false;
+    });
+
+
 
   });
 })(jQuery);
